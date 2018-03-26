2bfeb3eb3257309c4cef5e67b184d580c150e3c1
weiH <2236477952@qq.com>
2018-03-26 18:19:19 +0800

    add spe

--- src/main/java/com/example/configserver/ConfigserverApplication.java
+++ src/main/java/com/example/configserver/ConfigserverApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/*
+在spring cloud config 组件中，分两个角色，一是config server，二是config client。
+ */
+
 @SpringBootApplication
-@EnableConfigServer
+@EnableConfigServer   //@EnableConfigServer注解开启配置服务器的功能
 public class ConfigserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigserverApplication.class, args);
 	}
 }
+/*启动程序：访问http://localhost:8888/foo/dev
+显示json证明证明配置服务中心可以从远程程序获取配置信息。
+*/
